adbc993bb226be5c00e506675b9e67f4d03ca554
Kacper Lechicki <klechicki@poczta.fm>
2022-12-14 13:41:56 +0100

    show and hide panel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 'use stricts';
 
+const addBtn = document.querySelector('.add');
+const deleteAllBtn = document.querySelector('.deleteAll');
+const noteArea = document.querySelector('.note-area');
 
+const deleteNote = document.getElementsByClassName('.note-header-delete');
+
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+const notePanel = document.querySelector('.note-panel');
+const category = document.querySelector('#category');
+const textarea = document.querySelector('#text');
+const error = document.querySelector('.error');
+
+let selectedValue;
+
+let cardID = 0;
 
 //-----------------------------------------------
 //-----------------------------------------------
@@ -8,8 +23,13 @@
 //-----------------------------------------------
 //-----------------------------------------------
 
+const openPanel = () => {
+	notePanel.style.display = 'flex';
+};
 
-
+const closePanel = () => {
+	notePanel.style.display = 'none';
+};
 
 //-----------------------------------------------
 //-----------------------------------------------
@@ -17,4 +37,5 @@
 //-----------------------------------------------
 //-----------------------------------------------
 
-
+addBtn.addEventListener('click', openPanel);
+cancelBtn.addEventListener('click', closePanel);
